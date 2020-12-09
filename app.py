611b602f15096f208d17c757dbec1839f0e477bf
Elior Digmi <Eliory09@gmail.com>
2020-12-09 02:27:27 +0200

    Version 1.00

---
+++ app.py
@@ -0,0 +1,166 @@
+import os
+
+import bcrypt
+
+from dotenv import load_dotenv
+from flask import (Flask, abort, flash, redirect, render_template, request,
+                   session, url_for)
+from flask_login import (LoginManager, current_user, login_required,
+                         login_user, logout_user)
+from playhouse.shortcuts import model_to_dict
+
+from models import Difficulty, Riddles, Users, UsersRiddles, database
+
+
+load_dotenv()
+SECRET_KEY = os.getenv('PROJECT_API_KEY')
+
+
+app = Flask(__name__)
+app.secret_key = SECRET_KEY
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+MINIMAL_PASS_LENGTH = 8
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.get(user_id)
+
+
+def get_object_or_404(model, *expressions):
+    try:
+        return model.get(*expressions)
+    except model.DoesNotExist:
+        abort(404)
+
+
+@app.before_request
+def _db_connect():
+    database.connect()
+
+
+@app.teardown_request
+def _db_close(_):
+    if not database.is_closed():
+        database.close()
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.j2'), 404
+
+
+@app.errorhandler(401)
+def page_unauthorized(e):
+    return render_template('401.j2'), 401
+
+
+app.register_error_handler(404, page_not_found)
+app.register_error_handler(401, page_unauthorized)
+
+
+
+@app.route('/index')
+@app.route('/')
+def index():
+    return render_template("index.j2")
+
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        user = Users.select(Users.username).where(Users.username == request.form['username'])
+        if user:
+            flash('Username is already registered', "registered")
+        elif len(request.form['password']) < MINIMAL_PASS_LENGTH:
+            flash('Password must be at least 8 digits long')
+        else:
+            salt = bcrypt.gensalt(prefix=b'2b', rounds=10)
+            unhashed_password = request.form['password'].encode('utf-8')
+            hashed_password = bcrypt.hashpw(unhashed_password, salt)
+            fields = {
+                **request.form,
+                'password': hashed_password
+            }
+            user = Users(**fields)
+            user.save()
+            login_user(user)
+            return redirect(url_for('index'))
+    return render_template("register.j2")
+
+
+@app.route('/riddles/<int:riddle_id>', methods=["GET", "POST"])
+@login_required
+def riddle(riddle_id):
+    query = UsersRiddles.select(UsersRiddles.riddle_id).where(UsersRiddles.user_id == current_user.user_id)
+    solved = {riddle.riddle_id for riddle in query}
+    for num in solved:
+        if num == riddle_id:
+            return redirect(url_for('correct', riddle_id=riddle_id))
+    if request.method == "POST":
+        riddle = Riddles.select().where(Riddles.riddle_id == riddle_id).get()
+        answer = riddle.answer
+        if answer != request.form['user_answer']:
+            flash("Wrong answer. Try again.")
+        else:
+            UsersRiddles.create(riddle=riddle_id, user=current_user.user_id)
+            current_user.points += 1
+            current_user.save()
+            return redirect(url_for('correct', riddle_id=riddle_id))
+    riddle = Riddles.select().where(Riddles.riddle_id == riddle_id).join(Difficulty, attr='difficulty_id').get()
+    riddle_dict = model_to_dict(riddle)
+    return render_template("riddle.j2", **riddle_dict)
+
+
+@app.route('/riddles/<int:riddle_id>/correct', methods=["GET", "POST"])
+@login_required
+def correct(riddle_id):
+    return render_template("correct.j2", riddle_id=riddle_id, username=current_user.name)
+
+
+@app.route('/riddles')
+@login_required
+def riddles():
+    query = (Riddles
+            .select(Riddles.riddle_id)
+            .order_by(Riddles.riddle_id)
+            .limit(100))
+    riddles = [riddle.riddle_id for riddle in query]
+    query = UsersRiddles.select(UsersRiddles.riddle_id).where(UsersRiddles.user_id == current_user.user_id)
+    solved = {riddle.riddle_id for riddle in query}
+    return render_template("riddles.j2", riddles=riddles, solved=solved)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST' and request.form['username']:
+        try:
+            user = Users.select().where(
+                    Users.username == request.form['username']
+                ).get()
+        except Users.DoesNotExist:
+            flash('No such username found. Please try again')
+        else:
+            fields = model_to_dict(user)
+            pw = request.form['password'].encode('utf-8')
+            if bcrypt.checkpw(pw, fields['password'].encode('utf-8')):
+                user = Users(**fields)
+                login_user(user)
+                return redirect(url_for('index'))
+            else:
+                flash('The password entered is incorrect')
+
+    return render_template('login.j2')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    logout_user()
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run(threaded=True, port=5000)
---
+++ models.py
@@ -0,0 +1,99 @@
+from peewee import *
+import pandas
+from flask_login import UserMixin
+from random import randint
+
+
+database = SqliteDatabase('riddles.db')
+
+
+class UnknownField(object):
+    def __init__(self, *_, **__): pass
+
+
+class BaseModel(Model):
+    class Meta:
+        database = database
+
+
+class Difficulty(BaseModel):
+    difficulty_id = AutoField()
+    name = CharField()
+
+    class Meta:
+        table_name = 'difficulty'
+
+
+class Riddles(BaseModel):
+    answer = CharField()
+    difficulty = ForeignKeyField(column_name='difficulty_id', field='difficulty_id', model=Difficulty, null=True)
+    question = CharField()
+    riddle_id = AutoField()
+
+    class Meta:
+        table_name = 'riddles'
+
+
+class Users(BaseModel, UserMixin):
+    birthday = DateField()
+    email = CharField(null=True)
+    password = CharField()
+    points = IntegerField(constraints=[SQL("DEFAULT 0")])
+    user_id = AutoField()
+    username = CharField()
+    name = TextField()
+
+    class Meta:
+        table_name = 'users'
+
+
+class UsersRiddles(BaseModel):
+    riddle = ForeignKeyField(column_name='riddle_id', field='riddle_id', model=Riddles)
+    user = ForeignKeyField(column_name='user_id', field='user_id', model=Users)
+
+    class Meta:
+        table_name = 'users_riddles'
+        primary_key = False
+
+
+TABLES = [
+    Difficulty, Riddles, Users, UsersRiddles
+]
+
+
+def reset_db():
+    with database.connection_context():
+        database.create_tables(TABLES, safe=True)
+        database.commit()
+
+def update_difficulties():
+    with database.connection_context():
+        database.drop_tables(Difficulty)
+        database.create_tables([Difficulty], safe=True)
+        difficulties = [
+            {'name': 'Easy'},
+            {'name': 'Normal'},
+            {'name': 'Hard'}
+        ]
+        Difficulty.insert_many(difficulties).execute()
+        database.commit()
+
+
+    
+def update_riddles():
+    with database.connection_context():
+        database.create_tables([Riddles], safe=True)
+        data = pandas.read_csv('riddles.csv')
+        questions = data.QUESTIONS.tolist()
+        answers = data.ANSWERS.tolist()
+        qa_dict = dict(zip(questions, answers))
+        dicts_list = []
+        for k, v in qa_dict.items():
+            dicts_list.append({'question': k, 'answer': v, 'difficulty': randint(1, 3)})
+        Riddles.insert_many(dicts_list).execute()
+        database.commit()
+
+
+reset_db()
+update_difficulties()
+update_riddles()
